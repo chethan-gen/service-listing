1e15612c8fa4f698706970452a62e47699cccaf6
chethan-gen <chaithuregala123@gmailcom>
2025-04-28 15:07:34 +0530

    adding changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import {useState} from "react";
+import { useState } from "react";
 import ServiceCard from "./components/ServiceCard";
-import axios from "axios";
-import data from "./components/data.json"
+import data from "./components/data.json";
 
 function App() {
-   const {services,setServices} = useState({data});
+   const [services, setServices] = useState(data);
 
-   return(
-    <>
-    <div>
-    {services.map((service)=>(
-      <ServiceCard key={service.id} data={service}/>
-   ))}
-   </div>
+   return (
+      <>
+         <div>
+            {services.map((service) => (
+               <ServiceCard key={service.id} service={service} />
 
-   </>
-   
+            ))}
+         </div>
+      </>
    );
 }
 
 export default App;
-
-
